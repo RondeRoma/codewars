c3b7a00f2180f386fe80fd7e0cbac4a62b9368a6
Roman <4895476@gmail.com>
2020-10-15 01:52:47 +0300

    Добавляет код с кодворс и начинаем дз4

---
+++ js/dz4.js
@@ -0,0 +1,51 @@
+// // Write code under this line
+// const addIndex = (element, index) => element + index;// Write code under this line
+// const subIndex = (element, index) => element - index;
+  
+// function mapArray(array, cb) {
+//   'use strict';
+// // Write code under this line
+//   const numbers = new Array(array.length);
+//   for(let i = 0; i < array.length; i += 1) {
+//     const element = array[i];
+//     const index = i;
+//      numbers[i] = cb(element, index);
+//   }
+//   return numbers;
+// }
+
+// const arr  = [1,2,3,4,5];
+
+// console.log(mapArray(arr, addIndex));
+// // [1, 3, 5, 7, 9]
+
+// console.log(mapArray(arr, subIndex));
+// // [1, 1, 1, 1, 1]
+
+
+
+
+
+// //#2
+// const isUniq = (element, index, arr) => arr.indexOf(element) === index;
+// const isEven = (element) => element % 2 === 0;
+
+// function filterArray(array, cb) {
+//   'use strict';
+//   const numbers = [];
+//   for(let i = 0; i < array.length; i += 1) {
+//     const element = array[i];
+//     const index = i;
+//     // Write code under this line
+//     console.log(numbers);
+//   }
+//   return numbers;
+// }
+
+// const arr  = [1,2,3,4,5,1,2,5];
+
+// //console.log(filterArray(arr, isUniq));
+// // [1, 2, 3, 4, 5]
+
+// //console.log(filterArray(arr, isEven));
+// // [2, 4, 2]
--- js/index.js
+++ js/index.js
@@ -15,16 +15,47 @@
 //       return true;
 //   }
 
-function unique_Code(array) {
-    var result = null, n = array.length;
+// function findUniq(arr) {
+//     var result = null, n = arr.length;
   
-    for (let i = 0; i < n; i++) {
-      for (let v = i; v < n; v++) {
-        if (array[i] != array[v]) {
-          result = array[i];
-        }
-      }
-    }
+//     for (let i = 0; i < n; i++) {
+//       for (let v = i; v < n; v++) {
+//         if (arr[i] != arr[v]) {
+//           result = arr[i];
+//         }
+//       }
+//     }
   
-    return result;
-  }
+//     return result;
+//   }
+//   console.log(findUniq([ 0, 1, 0 ]), 1);
+
+//MIRRORS MIRRORS
+// function mirror(data) {
+//   let sortedArr = data.slice().sort((a,b) => a - b);
+//   let reversedArr = data.slice().sort((a,b) => b - a);
+//   return sortedArr.concat(reversedArr.slice(1));
+// }
+
+// function greet(name) {
+//   return `\x48\x65\x6c\x6c\x6f\x2c\x20${name}\x20\x68\x6f\x77\x20\x61\x72\x65\x20\x79\x6f\x75\x20\x64\x6f\x69\x6e\x67\x20\x74\x6f\x64\x61\x79\x3f`;
+// }
+
+// console.log(greet(Roma));
+
+const makeDish = function(sheffName, dish) {
+  console.log(`${sheffName} gotovit ${dish}`);
+};
+const makeSheff = function(name) {
+  return function(dish) {
+    console.log(`${name} gotovit ${dish}`);
+  };
+};
+
+const mango = makeSheff("Mango");
+const poly = makeSheff("Poly");
+
+mango('chuletas');
+poly('tea');
+poly('golubtsy');
+mango('borshch');
