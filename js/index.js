8f6c5aa1a1b5358d9355d56639fc8b5b2e46a5ad
Roman <4895476@gmail.com>
2020-11-08 00:28:06 +0200

    решает задачц 23 на кодворс

--- js/index.js
+++ js/index.js
@@ -226,3 +226,27 @@
 //   answer1 = '3743';
 // answer2 = 'UePJ';
 // answer3 = 'Nail scissors';
+
+const infiniteLoop = (arr,d,n) => {
+ 
+    // for (let i = 0; i <= arr.length; i+=1){
+        for (let j = 1; j <= n; j+=1){
+    if (d === "left"){
+      arr[2].push(arr[0].shift());
+      arr[1].push(arr[2].shift());
+      arr[0].push(arr[1].shift());
+    }
+    if (d === "right"){
+        arr[1].unshift(arr[0].pop());
+        arr[2].unshift(arr[1].pop());
+        arr[0].unshift(arr[2].pop());
+     
+    }
+}
+    
+    return arr;
+}
+  console.log(infiniteLoop( [[1,2,3],[4,5,6],[7,8,9]],"right",2));
+  console.log(infiniteLoop( [[1,2,3],[4,5,6],[7,8,9]],"left",1));
+    
+  
